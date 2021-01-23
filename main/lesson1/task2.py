c6e43a9589f12df9c3129b2720bafd0118ee45b0
Alexidis911 <Alexidis911@gmail.com>
2021-01-23 18:38:57 +0300

    Исправил второе задание

--- main/lesson1/task2.py
+++ main/lesson1/task2.py
@@ -6,16 +6,12 @@ def main():
     (x1, y1) = (int(x1), int(y1))
     (x2, y2) = (int(x2), int(y2))
 
-    x_coef = y1 - y2
-    x_str = f'{x_coef}x' if x_coef != 0 else ''
-
-    y_coef = x2 - x1
-    y_sign = '+' if y_coef > 0 else '-'
-    y_str = f'{y_sign} {abs(y_coef)}y' if y_coef != 0 else ''
-
-    free_coef = x1 * y2 - x2 * y1
-    free_sign = '+' if free_coef > 0 else '-'
-    free_str = f'{free_sign} {abs(free_coef)}' if free_coef != 0 else ''
-
-    equation = f'{x_str} {y_str} {free_str}'.strip()
-    print(equation)
+    if x1 == x2:
+        print('Прямая не может быть проведена через данные точки')
+    else:
+        k = (y2 - y1) / (x1 - x2)
+        b = (x2 * y1 - x1 * y2) / (x1 - x2)
+        b_sign = '+' if b > 0 else '-'
+        b_str = f'{b_sign} {abs(b)}' if b != 0 else ''
+        equation = f'{k}x {b_str}'.strip()
+        print(equation)
