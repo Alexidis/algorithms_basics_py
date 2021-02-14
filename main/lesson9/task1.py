1b544254f6e6b4371d08edd8b4379c5b6a195966
Alexidis911 <Alexidis911@gmail.com>
2021-02-14 10:07:02 +0300

    исправление первой задачи

--- main/lesson9/task1.py
+++ main/lesson9/task1.py
@@ -3,28 +3,22 @@
 # Примечания:
 # * в сумму не включаем пустую строку и строку целиком;
 # * без использования функций для вычисления хэша (hash(), sha1() или любой другой из модуля hashlib задача считается не решённой.
-import hashlib
 
 
 # генерируем матрицу смежности для двунаправленного графа
-def Rabim_karp(s: str, subs: str) -> int:
-    assert len(s) > 0 and len(subs) > 0, 'Строки не могут быть пустыми'
-    assert len(s) > len(subs), 'Полстрка длинее строки или ей'
+def hash_subsr_counter(s: str) -> int:
+    set_hash = set()
 
-    len_sub = len(subs)
-    h_subs = hashlib.sha1(subs.encode('utf-8')).hexdigest()
-    find_count = 0
+    for i in range(len(s) - 1):
+        for j in range(i + 1,  len(s) + 1):
+            set_hash.add(hash(s[i:j]))
 
-    for i in range(len(s) - len_sub + 1):
-        if h_subs == hashlib.sha1(s[i:i + len_sub].encode('utf-8')).hexdigest():
-            find_count += 1
-    return find_count
+    counter = len(set_hash)
+    return counter
 
 
 def main():
     s_1 = input('Введите строку: ')
-    s_2 = input('Введите подстроку для поиска: ')
-    find_count = Rabim_karp(s_1, s_2)
-    print(f'В строке "{s_1}" подстрока "{s_2}" встречается {find_count} раз')
 
-
+    count = hash_subsr_counter(s_1)
+    print(f'В строке "{s_1}" есть "{count}" подстрок')
